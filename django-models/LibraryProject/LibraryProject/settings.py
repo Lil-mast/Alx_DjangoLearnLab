16a4fe4a31a4d2ad7eee02d68d0126889902dc98
Taz <christiantazma77@gmail.com>
2025-08-09 14:58:53 +0300

    Implemented authentication

--- django-models/LibraryProject/LibraryProject/settings.py
+++ django-models/LibraryProject/LibraryProject/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'LibraryProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / "relationship_app/templates"], # Ensure this points to your templates directory
+        'DIRS': [BASE_DIR / "relationship_app" / "templates"], # Ensure this points to your templates directory
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,20 +1,15 @@
-from django.shortcuts import render
-from django.views.generic import DetailView
-from .models import Library, Book
-
-# Create your views here.
-from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
+from django.shortcuts import render
+from django.views.generic.detail import DetailView
+from .models import Book, Library
 
 # Function-based view to list all books
 def book_list_view(request):
     books = Book.objects.select_related('author').all()
-    output = "\n".join([f"{book.title} by {book.author.name}" for book in books])
-    return HttpResponse(output, content_type="text/plain")
+    return render(request, 'book_list.html', {'books': books})
 
-# Class-based view to show a library and its books
+# Class-based view to show library details and its books
 class LibraryDetailView(DetailView):
     model = Library
-    template_name = 'library_detail.html'  # Optional: we'll create this template
+    template_name = 'library_detail.html'
     context_object_name = 'library'
