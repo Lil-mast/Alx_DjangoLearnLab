dccb42e672a251ec8db329b3e38876788024c39c
Tazma <christiantazma77@gmail.com>
2025-08-25 22:48:32 +0300

    api test

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from .models import Book
 from .serializers import BookSerializer
 
@@ -11,3 +11,12 @@ class BookList(generics.ListAPIView):
     """
     queryset = Book.objects.all()  # Get all books from the database
     serializer_class = BookSerializer  # Use the BookSerializer for serialization
+
+# New ViewSet for complete CRUD operations
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    ViewSet that provides default `create()`, `retrieve()`, `update()`,
+    `partial_update()`, `destroy()` and `list()` actions for Book model.
+    """
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -44,6 +44,17 @@ INSTALLED_APPS = [
     'rest_framework',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10,
+    'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
+    'DEFAULT_PARSER_CLASSES': [
+        'rest_framework.parsers.JSONParser',
+    ],
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
