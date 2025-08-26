1f2d7d7db0e7842002c21359438ae1b432111158
Tazma <christiantazma77@gmail.com>
2025-08-26 12:48:22 +0300

    Fixed a bug

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
-from rest_framework import generics, permissions, filters
+from rest_framework import generics, permissions
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from rest_framework import generics, permissions, filters, status
 from rest_framework.response import Response
