fe271c5866303fade6b3d515f34a7fc17a68b705
Tazma <christiantazma77@gmail.com>
2025-08-27 18:23:46 +0300

    Fixed a bug in urls.py blog

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('post/', PostListView.as_view(), name='post_list'),
     path('post/new/', PostCreateView.as_view(), name='post_create'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
-    path('posts/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment_create'),
     path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post_update'),
     path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post_delete'),
 
