da1b5f0ec37497d27c71c53c959fc8cd5aa42895
Tazma <christiantazma77@gmail.com>
2025-08-29 17:04:23 +0300

    Social media api project setup

---
+++ social_media_api/accounts/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from django.contrib.auth import authenticate
+from .models import CustomUser
+
+class UserRegistrationSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, min_length=8)
+    password_confirm = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = CustomUser
+        fields = ('username', 'email', 'password', 'password_confirm', 'first_name', 'last_name')
+
+    def validate(self, data):
+        if data['password'] != data['password_confirm']:
+            raise serializers.ValidationError("Passwords don't match")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('password_confirm')
+        user = CustomUser.objects.create_user(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            password=validated_data['password'],
+            first_name=validated_data.get('first_name', ''),
+            last_name=validated_data.get('last_name', '')
+        )
+        return user
+
+class UserLoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField(write_only=True)
+
+    def validate(self, data):
+        username = data.get('username')
+        password = data.get('password')
+
+        if username and password:
+            user = authenticate(username=username, password=password)
+            if user:
+                if user.is_active:
+                    data['user'] = user
+                else:
+                    raise serializers.ValidationError("User account is disabled.")
+            else:
+                raise serializers.ValidationError("Unable to log in with provided credentials.")
+        else:
+            raise serializers.ValidationError("Must include 'username' and 'password'.")
+        return data
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    follower_count = serializers.ReadOnlyField()
+    following_count = serializers.ReadOnlyField()
+
+    class Meta:
+        model = CustomUser
+        fields = ('id', 'username', 'email', 'first_name', 'last_name', 
+                 'bio', 'profile_picture', 'follower_count', 'following_count',
+                 'created_at', 'updated_at')
+        read_only_fields = ('id', 'created_at', 'updated_at')
